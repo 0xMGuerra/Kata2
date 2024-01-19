2c802c60d2ca0b143698cccf40472dd886d34cb2
Micael <mguerrargz@gmail.com>
2024-01-19 20:29:59 +0000

    Created NetflixUserbaseProcessor interface and CountryNetflixUserbaseProcessor class.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        NetflixUserbaseLoader loader = new CsvFileNetflixUserbaseLoader(new File("Netflix Userbase.csv"));
+        List<NetflixUserbase> list = loader.load();
+        Map<String, Integer> process = new CountryNetflixUserbaseProcessor().process(list);
 
+        for(String key : process.keySet()) {
+            System.out.println(key + " -> " + process.get(key));
+        }
     }
 }
---
+++ src/NetflixUserbaseProcessor.java
@@ -0,0 +1,6 @@
+import java.util.List;
+import java.util.Map;
+
+public interface NetflixUserbaseProcessor {
+    Map<String, Integer> process(List<NetflixUserbase> list);
+}
